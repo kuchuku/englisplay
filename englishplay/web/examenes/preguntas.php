2a874b19227868675e801ae60d61aed91d1a559d
ashe <jorge.l.zapata@correounivalle.edu.co>
2018-03-06 19:35:12 -0500

    asdf
    cambios estilos, agregada carpeta examen

---
+++ englishplay/web/examenes/preguntas.php
@@ -0,0 +1,54 @@
+<?php 
+include("header.php");
+require("php/conexion.php");
+include("php/sesion.php");
+?>
+<?php 
+		 	$query ="SELECT id,pregunta,respuesta,elec1,elec2 FROM preguntas WHERE test = 0 AND capitulo = 1";
+		 	//Get result
+		 	$consulta = mysqli_query($conexion,$query);
+		 	$total = $consulta->num_rows;		
+		
+ ?>
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="css/preguntasForm.css" />
+<body>
+<main>
+	<div class="container">	
+		<br>		
+		<form method="post" class="formControl" action="resultados.php">
+		<?php while ($row = mysqli_fetch_array($consulta))  {
+		 	$idPregunta = $row["id"];
+		 	$textoPregunta = $row["pregunta"];
+		 	$num_pregunta;		 	
+		 	$ans_array = array($row['elec1'], $row['elec2'], $row['respuesta']); 
+		 	shuffle($ans_array);
+		 	$num_pregunta++;
+		 ?>
+	
+		<p class="question">
+		<?php echo '</p>
+			<div class="preguntas_text" align="justify"> 			
+			<label name="preguntaText">'."(".$idPregunta.") ".$textoPregunta.'</label>
+			</div>
+			<ul class="choices">
+				<li><input name="choice'.$idPregunta.'" type="radio" value="'.$ans_array[0].'" required/>'.$ans_array[0].'</li>
+				<li><input name="choice'.$idPregunta.'" type="radio" value="'.$ans_array[1].'"/>'.$ans_array[1].'</li>
+				<li><input name="choice'.$idPregunta.'" type="radio" value="'.$ans_array[2].'"/>'.$ans_array[2].'</li> 
+			</ul><br>	
+			';	
+		} 
+		 ?>
+		
+		<br>
+		<input type="submit" class="boton" value="Finalizar" />
+		<br>
+	</form>
+	</div>
+</main>
+</body>
+</html>
+<?php 
+include("footer.php");
+ ?>
---
+++ englishplay/web/examenes/resultados.php
@@ -0,0 +1,71 @@
+<?php 
+include("header.php");
+require("php/conexion.php");
+include("php/sesion.php");
+?>
+
+
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="css/preguntasForm.css" />
+<body>
+
+<main>
+	<div class="container">
+		<h2>Congrats! You have completed the test </h2>
+
+		<?php  
+			$query ="SELECT * FROM preguntas WHERE test = 0 AND capitulo = 1";
+		 	//Get result
+		 	$results = mysqli_query($conexion,$query); 	
+		 	$_SESSION['score'] = 0;
+
+		 	while ($row = mysqli_fetch_array($results)) 
+		 	{
+		 	$idprg = $row['id'];
+		 	$resp = $row['pregunta'];
+		 	$selected_choice = $_POST['choice'.$row['id']]; 	
+		 	
+		    	if (!isset($_SESSION['score'])) {
+					$_SESSION['score'] = 0;
+				}
+
+		    	if ($_POST) 
+		    	{	    			
+				 	//Obtener la respuesta correcta
+				 	$query ="SELECT respuesta FROM preguntas WHERE id='$questionID'";
+				 	//Get result
+				 	$respuesta = mysqli_query($conexion,$query); 
+		 			$correct_choice = $row ['respuesta'];
+		 		}
+		 		echo "(",$idprg,") ",$resp;
+		 		if ($correct_choice != $selected_choice) 
+		 		{
+				 	//answer is incorrect				 	
+				 	echo '<br>'."tu respuesta: ".'<span style="background-color: #FF9C9E">'.$selected_choice.'</span>';
+				 	echo '<br>'."respuesta correcta: ".'<span style="background-color: #ADFFB4">'.$correct_choice.'</span><br>';
+		 		}		
+		 		if ($correct_choice == $selected_choice) 
+		 		{
+				 	//answer is correct
+				 	$_SESSION['score']++;
+				 	echo '<br><p><span style="background-color: #ADFFB4">'.$selected_choice.'</span></p>';
+		 		}	
+		 	}
+		 	
+		?>	
+
+		
+		<a href="index.php" class="start">Continue</a>
+		<p>Final Score: <?php echo $_SESSION['score']; ?></p>
+		<p><span style="background-color: #FF9C9E"></span></p>
+	
+
+
+	</div>
+</main>
+</body>
+</html>
+<?php 
+include("footer.php");
+ ?>
