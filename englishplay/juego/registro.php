716f7a16747bb7707ed01bbb3175a06dd5f162fa
simon.naranjo.h <simon.naranjo.h@outlook.com>
2018-03-13 19:31:49 -0500

    fix registro.php

--- englishplay/juego/registro.php
+++ englishplay/juego/registro.php
@@ -7,13 +7,14 @@
 	$txtContrasenia	= $_GET['txtContrasenia'];
 
 	if(!$conexion->connect_errno) {
-		$sql 		= "SELECT nickEstudiante FROM estudiante WHERE codigoEstudiante = '$txtCodigo'";
+		$sql 		= "SELECT nickEstudiante FROM estudiante WHERE codigoUsuario = '$txtCodigo'";
 		$resultado	= mysqli_query($conexion, $sql);
 
 		if(mysqli_num_rows($resultado) == 1) {
 			$row = mysqli_fetch_assoc($resultado);
 			if($row["nickEstudiante"] == "") {
-				mysqli_query($conexion, "UPDATE estudiante SET nickEstudiante = '$txtNick', contraseniaEstudiante = '$txtContrasenia' WHERE codigoEstudiante = '$txtCodigo'");
+				mysqli_query($conexion, "UPDATE estudiante SET nickEstudiante = '$txtNick' WHERE codigoUsuario = '$txtCodigo'");
+				mysqli_query($conexion, "UPDATE usuario SET contraseniaUsuario = '$txtContrasenia' WHERE codigoUsuario = '$txtCodigo'");
 				echo 'OK';
 			}else {
 				echo 'estudianteEstabaRegistrado';
