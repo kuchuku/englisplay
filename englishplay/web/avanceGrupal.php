d222faa4ff50f8c31daadaf0c1133cc1573d0c77
simon.naranjo.h <simon.naranjo.h@outlook.com>
2018-03-19 20:12:19 -0500

    test graficas

--- englishplay/web/avanceGrupal.php
+++ englishplay/web/avanceGrupal.php
@@ -65,22 +65,94 @@ while ($row = mysqli_fetch_assoc($resultado))
 	}
 }
 
-$sql = "SELECT rolPersonaje, nivelPersonaje FROM personaje";
+$
+
+$numAvance1 = 0;
+$numAvance2 = 0;
+$numAvance3 = 0;
+$numAvance4 = 0;
+
+$sql = "SELECT mundoAvance FROM avance";
 $resultado = mysqli_query($conexion, $sql);
 while ($row = mysqli_fetch_assoc($resultado))
 {
+	
+	//Contador avance
+	if ($row[mundoAvance] == 1)
+	{
+		$numAvance1++;
+	}
+	else if ($row[mundoAvance] == 2)
+	{
+		$numAvance2++;
+	}
+	else if ($row[mundoAvance] == 3)
+	{
+		$numAvance3++;
+	}
+	else if ($row[mundoAvance] == 4)
+	{
+		$numAvance4++;
+	}
+}
 
+$numPreguntas;
+$numCorrectas;
+
+$sql = "SELECT preguntas, correctas FROM resultadoDesafio";
+$resultado = mysqli_query($conexion, $sql);
+while ($row = mysqli_fetch_assoc($resultado))
+{
+	$numPreguntas += $row[preguntas];
+	$numCorrectas += $row[correctas];
 }
 
 ?>
 
-<head>
+<html>
+  <head>
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
 
-</head>
+      // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
 
-<body>
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
 
-</body>
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Guerrero', <?php echo $numPersonajesGuerrero ?>],
+          ['Mago', <?php echo $numPersonajesArquero ?>],
+          ['Arquero', <?php echo $numPersonajesMago ?>]
+        ]);
+
+        // Set chart options
+        var options = {'title':'Personajes',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+
+  <body>
+    <!--Div that will hold the pie chart-->
+    <div id="chart_div"></div>
+  </body>
+</html>
 
 <?php
 
