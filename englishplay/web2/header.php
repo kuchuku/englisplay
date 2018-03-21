e82f46de5a0ef0f409a287fac354ec7b63453d74
ashe <jorge.l.zapata@correounivalle.edu.co>
2018-03-21 16:44:46 -0500

    :v
    :v:

--- englishplay/web2/header.php
+++ englishplay/web2/header.php
@@ -11,6 +11,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="vendor/bootstrap/css/styleForm.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -21,7 +22,6 @@
     <!-- JS -->
     <script type="text/javascript" src="js/jquery-1.10.1.min.js"></script>
 	<script type=" text/javascript" src="js/modernizr.custom.28468.js"></script>
-	<script src="js/jquery.flexslider-min.js"></script>
     <script>
 		$(function () {
 			var pul = $('#pull');
@@ -38,6 +38,12 @@
 	</script>
   </head>
   <header>
+	 <h1 class="site-heading text-center text-white d-none d-lg-block">
+      <span class="site-heading-upper text-primary mb-3">Learn while enjoying</span>
+      <span class="site-heading-lower">EnglishPlay</span>
+    </h1>
+
+
 	<nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
       <div class="container">
         <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
@@ -45,27 +51,29 @@
           <span class="navbar-toggler-icon"></span>
         </button>
          <div class="collapse navbar-collapse" id="navbarResponsive">
-		<ul class="navbar-nav mx-auto">			
+		<ul class="navbar-nav mx-auto">	
+		<li class='nav-item px-lg-4'><a href="index.php" id ="enlaces" class='nav-link text-uppercase text-expanded'>Inicio</a></li>		
 				<?php
 					error_reporting(E_ALL ^ E_NOTICE);
 					session_start();
 
 					if($_SESSION["autenticado"] & $_SESSION["estudiante"]){
-						echo "<li class="nav-item px-lg-4"><a class="nav-link text-uppercase text-expanded" href='examen.php' >Exam</a></li>";
-						echo "<li class="nav-item px-lg-4"><a class="nav-link text-uppercase text-expanded" href='student.php' >Student</a></li>";
-						echo "<li class="nav-item px-lg-4"><a class="nav-link text-uppercase text-expanded" href='estadisticas.php'>Results</a></li>";
+
+						echo "<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='examen.php'>Exam</a></li>";
+						echo "<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='student.php'>Student</a></li>";
+						echo "<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='estadisticas.php'>Results</a></li>";
 						?>
 					<?php
-					echo "<li class="nav-item px-lg-4"><a class="nav-link text-uppercase text-expanded" href='php/salir.php'>Salir</a></li>";
+					echo "<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='php/salir.php'>Salir</a></li>";
 					}
 					//EL ELSE IF ES LO DEL PROFESOR
 					elseif ($_SESSION["autenticado"] & !$_SESSION["estudiante"]) {
-						echo "<li class="nav-item px-lg-4"><a class="nav-link text-uppercase text-expanded" href='php/salir.php'>Salir</a></li>";
+						echo "<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='php/salir.php'>Salir</a></li>";
 					}
 
 
 					else{
-						echo "<li><a class="nav-link text-uppercase text-expanded"  href='login.php'>Login</a></li>";
+						echo "<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded'  href='login.php'>Login</a></li>";
 					}
 				?>
 				</ul>
