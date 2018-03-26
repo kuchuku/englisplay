4b47d2cdc44240d639271711e1668a4261d9b624
simon.naranjo.h <simon.naranjo.h@outlook.com>
2018-03-25 21:09:41 -0500

    phps edicion desafios

--- englishplay/web/editarDesafios.php
+++ englishplay/web/editarDesafios.php
@@ -10,14 +10,14 @@ include 'header.php';
 
 <body>
 
-	<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='Shooter.php'>Shooter</a></li>;
-	<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='Shooter.php'>Shooter</a></li>;
-	<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='Shooter.php'>Shooter</a></li>;
-	<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='Shooter.php'>Shooter</a></li>;
-	<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='Shooter.php'>Shooter</a></li>;
-	<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='Shooter.php'>Shooter</a></li>;
-	<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='Shooter.php'>Shooter</a></li>;
-	<li class='nav-item px-lg-4'><a class='nav-link text-uppercase text-expanded' href='Shooter.php'>Shooter</a></li>;
+	<a class='nav-link text-uppercase text-expanded' href='correctBox.php'>CorrectBox</a>
+	<a class='nav-link text-uppercase text-expanded' href='combat.php'>Combat</a>
+	<a class='nav-link text-uppercase text-expanded' href='plataformas.php'>Plataformas</a>
+	<a class='nav-link text-uppercase text-expanded' href='electroshock.php'>Electroshock</a>
+	<a class='nav-link text-uppercase text-expanded' href='enlazar.php'>Enlazar</a>
+	<a class='nav-link text-uppercase text-expanded' href='shooter.php'>Shooter</a>
+	<a class='nav-link text-uppercase text-expanded' href='laser.php'>Laser</a>
+	<a class='nav-link text-uppercase text-expanded' href='caminoCorrecto.php'>CaminoCorrecto</a>
 
 </body>
 
